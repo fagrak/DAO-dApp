2322a6ee12019619fa632cee48f7fa41a2108dff
Furkan <63971790+fagrak@users.noreply.github.com>
2023-03-28 01:50:24 +0300

    Initial Commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0xeeA21b8E05bf6decB4429D4cd6023975b77c2661";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
